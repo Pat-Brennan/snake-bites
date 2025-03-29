67a17f86efc8c979901a7f86fc3350d29a803887
Pat Brennan <patbrennan7@gmail.com>
2025-03-29 16:10:18 -0400

    created people script

---
+++ people.py
@@ -0,0 +1,15 @@
+
+people = {
+  'Mario': {
+    'first_name': 'Mario',
+    'last_name': 'Brother',
+    'childhood_dreams': 'To be a plumber in NYC'
+  },
+  'Luigi': {
+    'first_name': 'Luigi',
+    'last_name': 'Brother',
+    'childhood_dreams': 'For his brother to be proud of him'
+  },
+}
+
+print(people['Mario']['first_name'])
